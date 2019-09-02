d2d0acb5d6037787043fa7125f2c3290ba5082f3
playneed <huangjun@playneed.cn>
2019-09-02 15:39:55 +0800

    新增login页面

---
+++ pages/login/login.vue
@@ -0,0 +1,42 @@
+<template>
+	<view class="content" :style="{height:height + 'px'}">
+		<view class="top_oblique">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	var _this = this;
+	export default {
+		data() {
+			return {
+				height:0,
+			};
+		},
+		onLoad() {
+			_this = this;
+			uni.getSystemInfo({
+				success(res) {
+					_this.height = res.screenHeight
+				}
+			})
+		}
+	}
+</script>
+
+<style lang="scss">
+.content{
+	position: relative;
+	background:linear-gradient(left top, #6eccff , #a427b0);
+	.top_oblique{
+		width: 100%;
+		height: 350upx;
+		border-radius: 50upx;
+		background: #718bdf;
+		transform:rotate(45deg);
+		position: absolute;
+		top: -350upx;
+	}
+}
+</style>
